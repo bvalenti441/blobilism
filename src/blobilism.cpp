dc043f97ab01218411a66ccdbaae03faa8e48906
Ben Valenti <bvalenti@haverford.edu>
2023-01-24 20:46:27 -0500

    third commit

--- src/blobilism.cpp
+++ src/blobilism.cpp
@@ -12,17 +12,6 @@ struct Circle {
 };
 
 class MyWindow : public Window {
-private:
-
-    // todo: create member variables for 
-    // current circle size
-    int currBrushSize = 10;
-    // current transparency
-    float currTransparency = 1.0f;
-    // current color
-    float currColor[3];
-    // list of circles to draw each frame
-    std::vector<Circle> circles; // color, x position, y position, brush size
 
 public:
 
@@ -32,6 +21,7 @@ public:
     static const float blue[3];
     static const float yellow[3];
     static const float black[3];
+    static const float white[3];
     static const float gray[3];
 
     MyWindow(int w, int h) : Window(w, h) {}
@@ -59,7 +49,7 @@ public:
         if (currTransparency + change < 0){
             this->currTransparency = 0;
             return;
-        } 
+        }
         else if (currTransparency + change > 1.0f) {
             this->currTransparency = 1.0f;
             return;
@@ -91,8 +81,9 @@ public:
     }
 
     virtual void mouseMotion(int x, int y, int dx, int dy) {
-        if (mouseIsDown(GLFW_MOUSE_BUTTON_LEFT)) {
-            // todo: store a circle with the current color, size, x, y
+        // if holding left mouse button and not on the pallete
+        if (mouseIsDown(GLFW_MOUSE_BUTTON_LEFT) && mouseY() > 70) {
+            // stores a circle with the current color, size, x, y, and transparency
             Circle newCircle;
             float* circColor = getCurrColor();
             newCircle.color[0] = circColor[0];
@@ -104,6 +95,12 @@ public:
             newCircle.transparency = getTransparency();
             (*getCircles()).push_back(newCircle);
         }
+
+        // draw brush when mouse is moving
+        circle(mouseX(), mouseY(), 10);
+        color(black[0], black[1], black[2], 1.0f);
+        square(mouseX() + 50, mouseY(), 95, 10);
+        color(currColor[0], currColor[1], currColor[2], currTransparency);
     }
 
     virtual void mouseDown(int button, int mods) {
@@ -113,28 +110,33 @@ public:
             float my = mouseY();  // current mouse pos y
             
             // pallete circles
-            if (sqrt(pow(mx - (width() / 7.0f), 2) + pow(my - 35, 2)) <= 35) {
+            if (sqrt(pow(mx - (width() / 8.0f), 2) + pow(my - 35, 2)) <= 35) {
                 setCurrColor(red);
+                return;
             }
-            else if (sqrt(pow(mx - width() / 7.0f * 2.0f, 2) + pow(my - 35, 2)) <= 35) { 
+            else if (sqrt(pow(mx - width() / 8.0f * 2.0f, 2) + pow(my - 35, 2)) <= 35) { 
                 setCurrColor(green);
-                std::cout << "green" << std::endl;
+                return;
             }
-            else if (sqrt(pow(mx - width() / 7.0f * 3.0f, 2) + pow(my - 35, 2)) <= 35) { //pallette circles
+            else if (sqrt(pow(mx - width() / 8.0f * 3.0f, 2) + pow(my - 35, 2)) <= 35) { //pallette circles
                 setCurrColor(blue);
-                std::cout << "blue" << std::endl;
+                return;
             }
-            else if (sqrt(pow(mx - width() / 7.0f * 4.0f, 2) + pow(my - 35, 2)) <= 35) { //pallette circles
+            else if (sqrt(pow(mx - width() / 8.0f * 4.0f, 2) + pow(my - 35, 2)) <= 35) { //pallette circles
                 setCurrColor(yellow);
-                std::cout << "yellow" << std::endl;
+                return;
             }
-            else if (sqrt(pow(mx - width() / 7.0f * 5.0f, 2) + pow(my - 35, 2)) <= 35) { //pallette circles
+            else if (sqrt(pow(mx - width() / 8.0f * 5.0f, 2) + pow(my - 35, 2)) <= 35) { //pallette circles
                 setCurrColor(black);
-                std::cout << "black" << std::endl;
+                return;
             }
-            else if (sqrt(pow(mx - width() / 7.0f * 6.0f, 2) + pow(my - 35, 2)) <= 35) { //pallette circles
+            else if (sqrt(pow(mx - width() / 8.0f * 6.0f, 2) + pow(my - 35, 2)) <= 35) { //pallette circles
+                setCurrColor(white);
+                return;
+            }
+            else if (sqrt(pow(mx - width() / 8.0f * 7.0f, 2) + pow(my - 35, 2)) <= 35) { //pallette circles
                 setCurrColor(gray);
-                std::cout << "gray" << std::endl;
+                return;
             }
             else if (my <= 70) {
                 return;
@@ -149,6 +151,7 @@ public:
                 newCircle.diameter = getCurrBrushSize();
                 newCircle.transparency = getTransparency();
                 (*getCircles()).push_back(newCircle);
+                return;
             }
         }
     }
@@ -178,39 +181,60 @@ public:
 
     void draw() override {
 
-        background(0.95f, 0.95f, 0.95f); // parameters: r, g, b
+        background(white[0], white[1], white[2]);
 
-        // todo : draw pallette
+        // draws pallete
         color(0.1f, 0.1f, 0.1f);
         square(width()/2.0f, 35, width(), 70);
         
         color(red[0], red[1], red[2]);
-        circle(width() / 7.0f, 35, 35);
+        circle(width() / 8.0f, 35, 35);
         color(green[0], green[1], green[2]);
-        circle(width() / 7.0f * 2.0f, 35, 35);
+        circle(width() / 8.0f * 2.0f, 35, 35);
         color(blue[0], blue[1], blue[2]);
-        circle(width() / 7.0f * 3.0f, 35, 35);
+        circle(width() / 8.0f * 3.0f, 35, 35);
         color(yellow[0], yellow[1], yellow[2]);
-        circle(width() / 7.0f * 4.0f, 35, 35);
+        circle(width() / 8.0f * 4.0f, 35, 35);
         color(black[0], black[1], black[2]);
-        circle(width() / 7.0f * 5.0, 35, 1);
+        circle(width() / 8.0f * 5.0, 35, 1);
+        color(white[0], white[1], white[2]);
+        circle(width() / 8.0f * 6.0f, 35, 35);
         color(gray[0], gray[1], gray[2]);
-        circle(width() / 7.0f * 6.0f, 35, 35);
+        circle(width() / 8.0f * 7.0f, 35, 35);
 
         // draw all stored circles the user input
         for (Circle c : (*getCircles())) {
             color(c.color[0], c.color[1], c.color[2], c.transparency);
             circle(c.x, c.y, c.diameter);
         }
+
+        // draw brush when mouse is stationary
+        circle(mouseX(), mouseY(), 10);
+        color(black[0], black[1], black[2], 1.0f);
+        square(mouseX() + 50, mouseY(), 95, 10);
+        color(currColor[0], currColor[1], currColor[2], currTransparency);
     }
+
+    private:
+
+        // current circle size
+        int currBrushSize = 10;
+        // current transparency
+        float currTransparency = 1.0f;
+        // current color
+        float currColor[3];
+        // list of circles to draw each frame
+        std::vector<Circle> circles;
+
 };
 
-// initialize public color variables
+// initialize pallete colors
 const float MyWindow::red[3] = {1.0f, 0, 0};
 const float MyWindow::green[3] = { 0, 1.0f, 0 };
 const float MyWindow::blue[3] = { 0, 0, 1.0f };
 const float MyWindow::yellow[3] = { 1.0f, 1.0f, 0 };
 const float MyWindow::black[3] = { 0, 0, 0 };
+const float MyWindow::white[3] = { 0.95, 0.95, 0.95 };
 unsigned char grayHex = '0x80';
 const float MyWindow::gray[3] = { grayHex/255.0f, grayHex/255.0f, grayHex/255.0f };
 
